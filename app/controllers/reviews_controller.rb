1bebfa6e1cf0dd6db264b378413ff661e19fd1a8
zibs <elizibin@gmail.com>
2016-11-27 16:42:59 -0800

    working through kern

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,11 @@ class ReviewsController < ApplicationController
 
   def index
   end
-  
+
   def sxq
+  end
+  
+  def kern
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'reviews#index'
   get "Plausible-Hypotheses" => "reviews#sxq", as: :sxq
+  get "Kerncrete-Poetics" => "reviews#kern", as: :kern
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
