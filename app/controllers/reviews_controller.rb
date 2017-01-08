cdac9930484b7025079fb874dc38d1b54429822b
zibs <elizibin@gmail.com>
2017-01-07 19:51:49 -0800

    finish flashes. begin editing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,55 +4,62 @@ class ReviewsController < ApplicationController
   end
 
   def sxq
-    flash[:sxq] = "This review was originally written in 2015"
+    flash[:sxq] = "This review was originally written in 2015."
   end
 
   def kern
-    flash[:kern] = "This review was originally written in 2015"
+    flash[:kern] = "This review was originally written in 2015."
   end
 
   def their_bio
-    flash[:bio] = "This review was originally written in 2015"
+    flash[:bio] = "This review was originally written in 2015 and some of the links and screenshots may have now become out of date"
   end
 
   def revolving_door
-    flash[:door] = "This review was originally written in 2015"
   end
 
   def on_malice
-    flash[:malice] = "This review was originally written in 2015"
+    flash[:malice] = "This review was originally written in 2015."
   end
 
   def thrum
-    flash[:thrum] = "This review was originally written in 2015"
+    flash[:thrum] = "This review was originally written in 2015."
   end
 
   def consistently_round
+    flash[:round] = "This review was originally written in 2015."
   end
 
   def recalculating
-    flash[:recalculating] = "This review was originally written in 2015"
+    flash[:recalculating] = "This review was originally written in 2015."
   end
 
   def thin_meat
+    flash[:meat] = "This review was originally written in 2015."
   end
 
   def bardy_google
+    flash[:google] = "This review was originally written in 2015."
   end
 
   def forage
+    flash[:forage] = "This review was originally written in 2015."
   end
 
   def sympathy_loophole
+    flash[:loophole] = "This review was originally written in 2015."
   end
 
   def downverse
+    flash[:downverse] = "This review was originally written in 2015."
   end
 
   def think_tank
+    flash[:tank] = "This review was originally written in 2015."
   end
 
   def cut_up
+    flash[:cut] = "This review was originally written in 2015."
   end
 
 end
